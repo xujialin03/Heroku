dc3edf9b4279d988bc513645d77c69813ab515b9
xujialin03 <lin_7788@yeah.net>
2022-03-04 20:50:56 +0800

    3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 
 app.get("/",function(req,res){
-    res.setEncoding("working");
+    res.send("working");
 
 });
 app.listen(process.env.PORT || 5000);
