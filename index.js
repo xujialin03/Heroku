5ab26dcf93791e97bd32b2449e6bba65f0fffa8d
xujialin03 <lin_7788@yeah.net>
2022-03-05 20:42:50 +0800

    domain name

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 var express = require('express');
-var fs=require('fs');
 var app = express();
 var expressWs = require('express-ws')(app);
-var htmlfile=fs.readFileSync(__dirname+"/index.html","utf-8");
 var port=process.env.PORT || 3000;
-htmlfile=htmlfile.replace("{port}",port);
-console.log(htmlfile);
 app.use(function (req, res, next) {
   console.log('middleware');
-  // res.sendFile(__dirname+"/index.html");
-// res.end();
   req.testing = 'testing';
   return next();
 });
 
 app.get('/', function(req, res, next){
-  // console.log('get route', req.testing);
-  // res.end();
-  
-  res.send(htmlfile);
+  console.log('get route', req.testing);
+  res.end();
 });
-var count=0;
+
 app.ws('/', function(ws, req) {
   ws.on('message', function(msg) {
-    console.log(msg+count);
-    count++;
-    ws.send("msg"+count);
+    console.log(msg);
   });
   console.log('socket', req.testing);
 });
 
 app.listen(port);
-// }).listen(process.env.PORT || 5000);
+
+// var express = require('express');
+// var fs=require('fs');
+// var app = express();
+// var expressWs = require('express-ws')(app);
+
+// var htmlfile=fs.readFileSync(__dirname+"/index.html","utf-8");
+// var port=process.env.PORT || 3000;
+
+// app.use(function (req, res, next) {
+//   console.log('middleware');
+//   req.testing = 'testing';
+//   return next();
+// });
+
+// app.get('/', function(req, res, next){
+  
+//   res.send(htmlfile);
+// });
+// var count=0;
+// app.ws('/', function(ws, req) {
+//   ws.on('message', function(msg) {
+//     console.log(msg+count);
+//     count++;
+//     ws.send("msg"+count);
+//   });
+//   console.log('socket', req.testing);
+// });
+
+// app.listen(port);
