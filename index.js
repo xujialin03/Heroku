7fe3ef05e231bdf17c7d909b8748da47318f293c
xujialin03 <lin_7788@yeah.net>
2022-03-05 12:09:58 +0800

    domain name

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var express = require('express');
-var fs=require('fs');
 var app = express();
 var expressWs = require('express-ws')(app);
-console.log(__dirname+'/index.html');
-var html=fs.readFileSync(__dirname+'/index.html','utf-8');
-// console.log(html);
-var serverport=process.env.PORT || 3000;
-html=html.replace("{port}",serverport);
-console.log(html);
+
 app.use(function (req, res, next) {
   console.log('middleware');
   req.testing = 'testing';
@@ -16,30 +10,14 @@ app.use(function (req, res, next) {
 
 app.get('/', function(req, res, next){
   console.log('get route', req.testing);
-  //   res.end();
-//   res.sendFile(__dirname+'/index.html');
-res.send(html);
-//   console.log(__dirname+'/index.html');
-//   res.end();
+  res.end();
 });
-var count=0;
+
 app.ws('/', function(ws, req) {
   ws.on('message', function(msg) {
     console.log(msg);
-    count++;
-    ws.send("msg"+count);
   });
   console.log('socket', req.testing);
 });
 
-app.listen(serverport);
-
-
-// =ws.createServer(function(socket){
-//     var count =1;
-//     socket.on('text',function(str){
-//         console.log('str');
-//         socket.sendText('服务器收到客户端发来的消息!'+count++);
-//     });
-
-// }).listen(process.env.PORT || 5000);
+app.listen(3000);
