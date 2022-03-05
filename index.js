659d277f4bbf760f6e36882a2a2215e3c9dd7a5e
xujialin03 <lin_7788@yeah.net>
2022-03-05 19:48:26 +0800

    domain name

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var fs=require('fs');
 var app = express();
 var expressWs = require('express-ws')(app);
 var htmlfile=fs.readFileSync(__dirname+"/index.html","utf-8");
-
-htmlfile=htmlfile.replace("{port}",process.env.PORT || 3000);
+var port=process.env.PORT || 3000;
+htmlfile=htmlfile.replace("{port}",port);
 console.log(htmlfile);
 app.use(function (req, res, next) {
   console.log('middleware');
@@ -30,5 +30,5 @@ app.ws('/', function(ws, req) {
   console.log('socket', req.testing);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port);
 // }).listen(process.env.PORT || 5000);
