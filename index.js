27de4a2988b58b17993e46fedcfd88792b7d3908
xujialin03 <lin_7788@yeah.net>
2022-03-05 19:38:30 +0800

    domain name

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var fs=require('fs');
 var app = express();
 var expressWs = require('express-ws')(app);
-
+var html=fs.readFileSync(__dirname+"/index.html");
+html=html.replace("{port}",process.env.PORT || 3000);
 app.use(function (req, res, next) {
   console.log('middleware');
   // res.sendFile(__dirname+"/index.html");
@@ -13,12 +15,13 @@ app.use(function (req, res, next) {
 app.get('/', function(req, res, next){
   // console.log('get route', req.testing);
   // res.end();
+  
   res.sendFile(__dirname+"/index.html");
 });
 var count=0;
 app.ws('/', function(ws, req) {
   ws.on('message', function(msg) {
-    console.log(msg);
+    console.log(msg+count);
     count++;
     ws.send("msg"+count);
   });
